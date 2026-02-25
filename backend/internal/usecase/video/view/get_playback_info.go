d14fc27d2aba7120bde318eba4dda312c7696c80
nakamuraitsuki <ituki1911n@gmail.com>
2026-02-25 18:27:07 +0900

    feat: バックエンドから署名情報を入れたURLを発行

--- backend/internal/usecase/video/view/get_playback_info.go
+++ backend/internal/usecase/video/view/get_playback_info.go
@@ -2,9 +2,17 @@ package view
 
 import (
 	"context"
+	"crypto/md5"
+	"encoding/base64"
+	"fmt"
+	"time"
 
 	video_value "example.com/m/internal/domain/video/value"
 	"github.com/google/uuid"
+)
+
+const (
+	secretKey = "secret" // 本番環境では安全な方法で管理すること
 )
 
 type PlaybackInfo struct {
@@ -30,19 +38,22 @@ func (uc *VideoViewingUseCase) GetPlaybackInfo(
 		return nil, ErrVideoForbidden
 	}
 
-	url := "/api/videos/" + videoID.String() + "/stream/"
+	expires := time.Now().Add(2 * time.Hour).Unix()
+	vid := videoID.String()
 
-	// url, err := uc.Storage.GenerateTemporaryAccessURL(
-	// 	ctx,
-	// 	video.StreamKey(),
-	// 	15*time.Minute,
-	// )
-	// if err != nil {
-	// 	return nil, err
-	// }
+	hash := uc.generateHash(vid, expires, secretKey)
+
+	// signedPath の組み立て順序を HASH / EXPIRES / VIDEO_ID に変更
+	signedPath := fmt.Sprintf("/api/videos/%s/%d/%s/stream/index.m3u8", hash, expires, vid)
 
 	return &PlaybackInfo{
-		PlaybackURL: url,
+		PlaybackURL: signedPath,
 		MIMEType:    "application/x-mpegURL",
 	}, nil
+}
+
+func (uc *VideoViewingUseCase) generateHash(videoID string, expires int64, secret string) string {
+	input := fmt.Sprintf("%d%s%s", expires, videoID, secret)
+	h := md5.Sum([]byte(input))
+	return base64.RawURLEncoding.EncodeToString(h[:])
 }
